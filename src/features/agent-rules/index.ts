fc1d6c8ec010074466a7950f93bc620fd4617807
uprising <danielkajewski@outlook.com>
2026-04-02 23:58:37 -0500

    feat(agent-rules): add types, constants, and barrel exports

---
+++ src/features/agent-rules/index.ts
@@ -0,0 +1,8 @@
+export type { AgentRule, AgentRuleMetadata, AgentRulesContext } from "./types";
+export {
+  AGENT_RULES_AGENTS_SUBDIR,
+  AGENT_RULES_CATEGORIES_SUBDIR,
+  AGENT_RULES_CONVENTION_DIRS,
+  AGENT_RULES_FILE_EXTENSIONS,
+  AGENT_RULES_USER_DIRS,
+} from "./constants";
